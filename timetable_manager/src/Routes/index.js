297929dc1d075c6d2b7c3fb7b257d62eb26a7fa6
chathurya98 <chathuryakumarapperuma@gmail.com>
2021-04-05 20:53:13 +0530

    All component nvigation memeber 4 ,sprint1

--- timetable_manager/src/Routes/index.js
+++ timetable_manager/src/Routes/index.js
@@ -13,88 +13,111 @@ import ManageStudent from '../funtions/Student/ManageStudent';
 import AddTags from '../funtions/Tag/AddTags';
 import ManageTags from '../funtions/Tag/ManageTags';
 
+import Addworkingdays from '../funtions/working days and hours/Addworkingdays';
+import Managetimeslot from '../funtions/working days and hours/Managetimeslot';
+import Manageworkingdays from '../funtions/working days and hours/Manageworkingdays';
+
+
 
 let routes = [
 
-  {
-    path: "/",
-    name: "Dashboard",
-    component: Dashboard,
-    exact: true,
-  },
-  {
-    path: "/building/add",
-    name: "ADD_BUILDING",
-    component: AddBuilding,
-    exact: true,
-  },
-  {
-    path: "/building/manage",
-    name: "MANAGE_BUILDING",
-    component: ManageBuilding,
-    exact: true,
-  },
-  {
-    path: "/room/add",
-    name: "ADD_ROOM",
-    component: AddRoom,
-    exact: true,
-  },
-  {
-    path: "/room/manage",
-    name: "MANAGE_ROOM",
-    component: ManageRoom,
-    exact: true,
-  },
-  {
-    path: "/statistics/student",
-    name: "STUDENT_STATISTICS",
-    component: StudentStatistics,
-    exact: true,
-  },
-  {
-    path: "/statistics/lecturer",
-    name: "LECTURER_STATISTICS",
-    component: LecturerStatistics,
-    exact: true,
-  },
-  {
-    path: "/statistics/subject",
-    name: "SUBJECT_STATISTICS",
-    component: SubjectStatistics,
-    exact: true,
-  },
+    {
+        path: "/",
+        name: "Dashboard",
+        component: Dashboard,
+        exact: true,
+    },
+    {
+        path: "/building/add",
+        name: "ADD_BUILDING",
+        component: AddBuilding,
+        exact: true,
+    },
+    {
+        path: "/building/manage",
+        name: "MANAGE_BUILDING",
+        component: ManageBuilding,
+        exact: true,
+    },
+    {
+        path: "/room/add",
+        name: "ADD_ROOM",
+        component: AddRoom,
+        exact: true,
+    },
+    {
+        path: "/room/manage",
+        name: "MANAGE_ROOM",
+        component: ManageRoom,
+        exact: true,
+    },
+    {
+        path: "/statistics/student",
+        name: "STUDENT_STATISTICS",
+        component: StudentStatistics,
+        exact: true,
+    },
+    {
+        path: "/statistics/lecturer",
+        name: "LECTURER_STATISTICS",
+        component: LecturerStatistics,
+        exact: true,
+    },
+    {
+        path: "/statistics/subject",
+        name: "SUBJECT_STATISTICS",
+        component: SubjectStatistics,
+        exact: true,
+    },
 
-  {
-    path: "/student/add",
-    name: "ADD_STUDENT",
-    component: AddStudent,
-    exact: true,
-  },
+    {
+        path: "/student/add",
+        name: "ADD_STUDENT",
+        component: AddStudent,
+        exact: true,
+    },
 
-  {
-    path: "/student/manage",
-    name: "MANAGE_STUDENT",
-    component: ManageStudent,
-    exact: true,
-  },
+    {
+        path: "/student/manage",
+        name: "MANAGE_STUDENT",
+        component: ManageStudent,
+        exact: true,
+    },
 
-  {
-    path: "/tag/add",
-    name: "ADD_TAG",
-    component: AddTags,
-    exact: true,
-  },
+    {
+        path: "/tag/add",
+        name: "ADD_TAG",
+        component: AddTags,
+        exact: true,
+    },
 
-  {
-    path: "/tag/manage",
-    name: "MANAGE_TAG",
-    component: ManageTags,
-    exact: true,
-  },
+    {
+        path: "/tag/manage",
+        name: "MANAGE_TAG",
+        component: ManageTags,
+        exact: true,
+    },
 
+    {
+        path: "/workingday/add",
+        name: "ADD_WORKING_DAY",
+        component: Addworkingdays,
+        exact: true,
+    },
 
+    {
+        path: "/workingday/manage",
+        name: "MANAGE_WORKING_DAY",
+        component: Manageworkingdays,
+        exact: true,
+    },
 
+    {
+        path: "/timeslot/manage",
+        name: "MANAGE_TIMESLOT",
+        component: Managetimeslot,
+        exact: true,
+    },
 ];
 
 export default routes;
---
+++ timetable_manager/src/funtions/working days and hours/Managetimeslot.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Sidebar from '../../components/Sidebar'
+// import {FormInput , FormSelect , MultiFormSelect} from '../../Components/Form'
+
+class Managetimeslot extends React.Component {
+
+
+    render() {
+        // const {errors} = this.state;
+        return ( <
+            >
+            <
+            Sidebar / >
+            <
+            main >
+            <
+            h1 > Hi manage time slot < /h1> <
+            /main> <
+            />
+        );
+    }
+
+}
+
+
+
+
+export default Managetimeslot;
