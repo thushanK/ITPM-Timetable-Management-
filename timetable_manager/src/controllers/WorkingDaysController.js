9eb53a809c7a982cae7ada22bac0c08bc424093b
chathurya98 <chathuryakumarapperuma@gmail.com>
2021-04-08 10:08:26 +0530

    working days server side completed

--- timetable_manager/src/controllers/WorkingDaysController.js
+++ timetable_manager/src/controllers/WorkingDaysController.js
@@ -1,84 +1,83 @@
 import axios from "axios";
-import Config from "./Config.Controller";
+import Config from "./Config.controller";
 //done
 export const getWorkingDays = () => {
-    return new Promise( (resolve,reject) => {
-        return axios.get(`${Config.host}${Config.port}/api/workingdays/get`)
-            .then( result => {
-               if(result.status == 200){
-                    resolve(result.data)
-               }else{
-                resolve([])
-               }
-            })
-            .catch( err => {
-                reject(err)
-            })
-    })
-}
-//done
+        return new Promise((resolve, reject) => {
+            return axios.get(`${Config.host}${Config.port}/api/workingdays/get`)
+                .then(result => {
+                    if (result.status == 200) {
+                        resolve(result.data)
+                    } else {
+                        resolve([])
+                    }
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+    //done
 export const getWorkingDaysSingle = id => {
-    return new Promise( (resolve,reject) => {
-        return axios.get(`${Config.host}${Config.port}/api/workingdays/get/${id}`)
-            .then( result => {
-               if(result.status == 200){
-                    resolve(result.data)
-               }else{
-                resolve([])
-               }
-            })
-            .catch( err => {
-                reject(err)
-            })
-    })
-}
-//done
+        return new Promise((resolve, reject) => {
+            return axios.get(`${Config.host}${Config.port}/api/workingdays/get/${id}`)
+                .then(result => {
+                    if (result.status == 200) {
+                        resolve(result.data)
+                    } else {
+                        resolve([])
+                    }
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+    //done
 export const insertGroup = (data) => {
-    return new Promise( (resolve,reject) => {
-        return axios.post(`${Config.host}${Config.port}/api/workingdays/add` , data)
-            .then( result => {
-                if(result.status == 200){
-                    resolve({...result.data , status : 200})
-               }else{
-                resolve({ status : result.status })
-               }
+    return new Promise((resolve, reject) => {
+        return axios.post(`${Config.host}${Config.port}/api/workingdays/add`, data)
+            .then(result => {
+                if (result.status == 200) {
+                    resolve({...result.data, status: 200 })
+                } else {
+                    resolve({ status: result.status })
+                }
             })
-            .catch( err => {
+            .catch(err => {
                 reject(err);
             })
     })
 }
 
 export const updateGroup = (data) => {
-    return new Promise( (resolve,reject) => {
-        return axios.post(`${Config.host}${Config.port}/api/workingdays/update` , data)
-            .then( result => {
-                if(result.status == 200){
-                    resolve({...result.data , status : 200})
-               }else{
-                resolve({ status : result.status })
-               }
-            })
-            .catch( err => {
-                reject(err);
-            })
-    })
-}
-//done
+        return new Promise((resolve, reject) => {
+            return axios.post(`${Config.host}${Config.port}/api/workingdays/update`, data)
+                .then(result => {
+                    if (result.status == 200) {
+                        resolve({...result.data, status: 200 })
+                    } else {
+                        resolve({ status: result.status })
+                    }
+                })
+                .catch(err => {
+                    reject(err);
+                })
+        })
+    }
+    //done
 export const deleteGroup = id => {
     console.log(id)
-    return new Promise( (resolve,reject) => {
-        return axios.delete(`${Config.host}${Config.port}/api/workingdays/delete` , {data : {id : id }})
-            .then( result => {
-                if(result.status == 200){
-                    resolve({...result.data , status : 200})
-               }else{
-                resolve({ status : result.status })
-               }
+    return new Promise((resolve, reject) => {
+        return axios.delete(`${Config.host}${Config.port}/api/workingdays/delete`, { data: { id: id } })
+            .then(result => {
+                if (result.status == 200) {
+                    resolve({...result.data, status: 200 })
+                } else {
+                    resolve({ status: result.status })
+                }
             })
-            .catch( err => {
+            .catch(err => {
                 reject(err);
             })
     })
 }
-
